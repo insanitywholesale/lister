3c6c93e35c19a9e6659dd68140cd878938c3b6e0
lhz <pepitsizous@zoho.com>
2021-06-08 18:21:40 +0300

    implementation stubs for grpc

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -1,9 +1,22 @@
 package grpc
 
 import (
+	"context"
 	pb "gitlab.com/insanitywholesale/lister/proto/v1"
 )
 
 type Server struct {
 	pb.UnimplementedListerServer
+}
+
+func (Server) GetAllLists(context.Context, *pb.Empty) (*pb.Lists, error) {
+	return nil, nil
+}
+
+func (Server) GetList(_ context.Context, _ *pb.List) (*pb.List, error) {
+	return nil, nil
+}
+
+func (Server) AddList(_ context.Context, _ *pb.List) (*pb.Lists, error) {
+	return nil, nil
 }
